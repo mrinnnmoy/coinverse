52f16c3c2e3526e9ab9ac1c38259ac55fc598cb8
mrinnnmoy <mrinmoy202000@gmail.com>
2023-01-29 11:16:27 +0530

    build signup-section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from './components/Featured/Featured';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import Signup from './components/Signup/Signup';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Featured />
+      <Signup />
     </div>
   )
 };
